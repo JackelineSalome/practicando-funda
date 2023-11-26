0ad774b5e691065d4bb20de1b795a9c0902d25c6
JackelineSalome <89036070+JackelineSalome@users.noreply.github.com>
2023-11-26 09:31:07 -0500

    new commit

--- src/main/java/com/docseeker/MedicalHistory/MedicalHistoryApplication.java
+++ src/main/java/com/docseeker/MedicalHistory/MedicalHistoryApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MedicalHistoryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MedicalHistoryApplication.class, args);
+
+    SpringApplication.run(MedicalHistoryApplication.class, args);
 	}
 
 }
